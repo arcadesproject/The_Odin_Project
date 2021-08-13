83a802454f8e94b5f69bb4952771c758287e2dc9
arcadesproject <arcadesprojectcode@gmail.com>
2021-08-13 21:14:10 +0100

    refactored to use async/await instead of promises

--- Javascript/APIs/main.js
+++ Javascript/APIs/main.js
@@ -2,16 +2,35 @@ const img = document.getElementById('image');
 const button = document.getElementById('new');
 const search = document.getElementById('search');
 
-function newImg(input = 'error 404') {
-  fetch(
+// function newImg(input = 'error 404') {
+//   fetch(
+//     `https://api.giphy.com/v1/gifs/translate?api_key=AnQ1mlCxEbQt1XqjwPTGTIIZK8MQhfQu&s=${input}`,
+//     {
+//       mode: 'cors',
+//     },
+//   )
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (response) {
+//       img.src = response.data.images.original.url;
+//       img.dataset.info = `${input}`;
+//     })
+//     .catch(function (error) {
+//       newImg();
+//       console.log(error);
+//     });
+// }
+
+async function newImg(input = 'error 404') {
+  const response = await fetch(
     `https://api.giphy.com/v1/gifs/translate?api_key=AnQ1mlCxEbQt1XqjwPTGTIIZK8MQhfQu&s=${input}`,
     {
       mode: 'cors',
     },
-  )
-    .then(function (response) {
-      return response.json();
-    })
+  );
+  const data = await response
+    .json()
     .then(function (response) {
       img.src = response.data.images.original.url;
       img.dataset.info = `${input}`;
