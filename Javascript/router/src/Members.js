be10c81f502c38e9b04eec5589aee5a1111d202f
arcadesproject <arcadesprojectcode@gmail.com>
2022-01-26 16:57:59 +0000

    added members route

---
+++ Javascript/router/src/Members.js
@@ -0,0 +1,9 @@
+const Members = () => {
+  return (
+    <div>
+      <h2>Hello from members</h2>
+    </div>
+  );
+};
+
+export default Members;
--- Javascript/router/src/RouteSwitch.js
+++ Javascript/router/src/RouteSwitch.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Profile from './Profile';
+import Members from './Members';
 
 const RouteSwitch = () => {
   return (
@@ -8,6 +9,7 @@ const RouteSwitch = () => {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/members" element={<Members />} />
       </Routes>
     </BrowserRouter>
   );
